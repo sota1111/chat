2b1ad2f34b39390dd9d308958d59bf840b1082e6
sota1111 <sota.moro@gmail.com>
2023-06-22 12:56:43 +0900

    とりあえず動作。たまにFailする。原因調査中。

--- chat_dio/lib/chat_dio.dart
+++ chat_dio/lib/chat_dio.dart
@@ -17,7 +17,7 @@ class SecondChatRoom extends StatefulWidget {
 class ChatRoomState extends State<SecondChatRoom> {
   final List<types.Message> _messages = [];
   final _user = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d6f3ac');
-  types.User _dio = const types.User(
+  final types.User _dio = const types.User(
     id: 'dio',
     firstName: "Dio",
     lastName: "",
@@ -39,7 +39,7 @@ class ChatRoomState extends State<SecondChatRoom> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Chat with Dio')),
+      appBar: AppBar(title: const Text('教えてディオ課長')),
       drawer: const AppDrawer(),
       body: Chat(
         user: _user,
@@ -49,22 +49,6 @@ class ChatRoomState extends State<SecondChatRoom> {
         showUserNames: true,
       ),
     );
-  }
-
-  void _addNetworkImageAsMessage(String imageUrl) {
-    final message = types.ImageMessage(
-      author: _user,
-      createdAt: DateTime.now().millisecondsSinceEpoch,
-      id: randomString(),
-      // ここで他のプロパティを設定する必要があるかもしれません
-      uri: imageUrl,
-      name: 'image', // 画像の名前 (任意で設定)
-      size: 1, // 画像のサイズ (必要に応じて設定)
-      width: 100, // 画像の幅 (必要に応じて設定)
-      height: 100, // 画像の高さ (必要に応じて設定)
-    );
-
-    _addMessage(message);
   }
 
   void _addMessage(types.Message message) {
@@ -87,18 +71,9 @@ class ChatRoomState extends State<SecondChatRoom> {
 
     Future.delayed(const Duration(seconds: 1), () {
       final responseText = apiResponseData['Response'] ?? 'Failed';
-      final quote = apiResponseData['MaxIndex_Emotion'] ?? '0';
-      final quoteNumber = apiResponseData['MaxIndex_Emotion'] ?? '0';
-      final quoteUrl = apiResponseData['MaxIndex_Emotion'] ?? '0';
-      print(quoteNumber);
-
-      final responseMessage = types.TextMessage(
-        author: _dio,
-        createdAt: DateTime.now().millisecondsSinceEpoch,
-        id: randomString(),
-        text: responseText,
-      );
-      _addMessage(responseMessage);
+      final quote = apiResponseData['Quote'] ?? 'false';
+      final quoteUrl = apiResponseData['Url'] ?? '';
+      debugPrint(quoteUrl);
 
       // 画像メッセージを送信
       if(quote == "true"){
@@ -113,13 +88,21 @@ class ChatRoomState extends State<SecondChatRoom> {
           height: 70,
         );
         _addMessage(imageMessage);
+      }else{
+        final responseMessage = types.TextMessage(
+          author: _dio,
+          createdAt: DateTime.now().millisecondsSinceEpoch,
+          id: randomString(),
+          text: responseText,
+        );
+        _addMessage(responseMessage);
       }
     });
   }
 
   Future<Map<String, dynamic>> fetchResponseFromApi(String inputText) async {
     const String url =
-        'https://as7ol49p7g.execute-api.ap-northeast-1.amazonaws.com/Prod/chatroom';
+        'https://u5fhd9aj1l.execute-api.ap-northeast-1.amazonaws.com/Prod/chat-comic';
     final Map<String, String> headers = {'Content-Type': 'application/json'};
     final Map<String, String> data = {
       'input_text': inputText,
@@ -130,15 +113,15 @@ class ChatRoomState extends State<SecondChatRoom> {
 
     if (response.statusCode == 200) {
       var jsonResponse = json.decode(response.body);
+      debugPrint(jsonResponse['Response']);
+      debugPrint(jsonResponse['Quote']);
       return {
         'Response': jsonResponse['Response'],
-        'Emotion': jsonResponse['Emotion']['Emotion'],
-        'MaxIndex_Emotion': jsonResponse['Emotion']['MaxIndex'],
-        'NeedMap': jsonResponse['Map']['Need'],
-        'FloorUrl': jsonResponse['Map']['Url'],
+        'Quote': jsonResponse['Quote'],
+        'Url': jsonResponse['Url'],
       };
     } else {
-      return {'Response': 'Failed', 'Emotion': '[嬉しい:0, 心配:0, 困惑:0, 懸命:0]', 'MaxIndex_Emotion': '0'};
+      return {'Response': 'Failed', 'Quote': '0', 'Url': ''};
     }
   }
 }
--- chat_dio/lib/utilities.dart
+++ chat_dio/lib/utilities.dart
@@ -13,6 +13,6 @@ class ImageUrls {
   static const hospiFace2 = "https://images-hospi.s3.ap-northeast-1.amazonaws.com/Face02.jpg";
   static const hospiFace3 = "https://images-hospi.s3.ap-northeast-1.amazonaws.com/Face03.jpg";
 
-  static const dioFace0 = "https://images-dio.s3.ap-northeast-1.amazonaws.com/Face00.jpg";
+  static const dioFace0 = "https://images-dio.s3.ap-northeast-1.amazonaws.com/face.png";
 }
 
