c4a0a4081c74ee49b415ad108fb9f90b1f1dbd4a
sota1111 <sota.moro@gmail.com>
2023-07-21 12:33:10 +0900

    llama_indexでサーバ動作確認 応答がunicodeで返る

--- chat_docker/chat-docker/hello_world/app.py
+++ chat_docker/chat-docker/hello_world/app.py
@@ -3,32 +3,44 @@ import os
 import boto3
 import openai
 
-from llama_index import SimpleDirectoryReader
-from llama_index import Document
-from llama_index import GPTListIndex
+from dotenv import load_dotenv
+from langchain import OpenAI
+from llama_index import VectorStoreIndex, SimpleDirectoryReader
+from llama_index import StorageContext, load_index_from_storage
 
 from dotenv import load_dotenv
 load_dotenv()
-
 API_KEY_ENV_NAME = 'API_Key'
 OPENAI_API_KEY = os.environ[API_KEY_ENV_NAME]
 openai.api_key = OPENAI_API_KEY
 
 def lambda_handler(event, context):
-    documents = SimpleDirectoryReader(input_dir="./data").load_data()
-    print("documents: ", documents)
-    list_index = GPTListIndex.from_documents(documents)
-    print("list_index: ", list_index)
-    query_engine = list_index.as_query_engine()
-    print("query_engine: ", query_engine)
-    response = query_engine.query("機械学習に関するアップデートについて300字前後で要約してください。")
+    # モデルの読み込み
+    storage_context = StorageContext.from_defaults(persist_dir='./storage')
+    index = load_index_from_storage(storage_context)
+
+    # イベントからメッセージを取得
+    try:
+        message = json.loads(event['body'])['message']
+    except KeyError:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "message": "Invalid input. 'message' key is required."
+            })
+        }
+
+    # クエリの実行
+    query_engine = index.as_query_engine()
+    response = query_engine.query(message)
     print("response: ", response)
+    response = str(response)
 
     return {
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "hello world",
+                "message": response,
             }
         ),
     }
