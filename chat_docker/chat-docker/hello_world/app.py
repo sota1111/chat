8ac24893cb505875902db4feaa74f380c68bc213
sota1111 <sota.moro@gmail.com>
2023-07-21 15:49:12 +0900

    リファクタリング

--- chat_docker/chat-docker/hello_world/app.py
+++ chat_docker/chat-docker/hello_world/app.py
@@ -8,39 +8,44 @@ from langchain import OpenAI
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
 from llama_index import StorageContext, load_index_from_storage
 
-from dotenv import load_dotenv
 load_dotenv()
 API_KEY_ENV_NAME = 'API_Key'
-OPENAI_API_KEY = os.environ[API_KEY_ENV_NAME]
+OPENAI_API_KEY = os.getenv(API_KEY_ENV_NAME)
 openai.api_key = OPENAI_API_KEY
 
-def lambda_handler(event, context):
-    # モデルの読み込み
-    storage_context = StorageContext.from_defaults(persist_dir='./storage')
-    index = load_index_from_storage(storage_context)
-
-    # イベントからメッセージを取得
+def get_message_from_event(event):
     try:
-        message = json.loads(event['body'])['message']
+        return json.loads(event['body'])['input_text']
     except KeyError:
-        return {
-            "statusCode": 400,
-            "body": json.dumps({
-                "message": "Invalid input. 'message' key is required."
-            })
-        }
+        raise ValueError("Invalid input. 'message' key is required.")
 
-    # クエリの実行
-    query_engine = index.as_query_engine()
-    response = query_engine.query(message)
-    print("response: ", response)
-    response = str(response)
+def generate_error_response(message):
+    return {
+        "statusCode": 400,
+        "body": json.dumps({
+            "message": message
+        })
+    }
 
+def generate_success_response(response):
     return {
         "statusCode": 200,
-        "body": json.dumps(
-            {
-                "message": response,
-            }
-        ),
+        "body": json.dumps({
+            "Response": str(response),
+        }),
     }
+
+def lambda_handler(event, context):
+    try:
+        message = get_message_from_event(event)
+    except ValueError as error:
+        return generate_error_response(str(error))
+
+    storage_context = StorageContext.from_defaults(persist_dir='./storage')
+    index = load_index_from_storage(storage_context)
+    query_engine = index.as_query_engine()
+
+    response = query_engine.query(message)
+    print("response: ", response)
+
+    return generate_success_response(response)
